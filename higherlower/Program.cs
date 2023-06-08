fe36c88797a1943ac8827b795f677ace733a14f2
hzno123 <63207211+hzno123@users.noreply.github.com>
2023-06-08 10:17:34 +0100

    Add project files.

---
+++ higherlower/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Security.Cryptography;
+
+namespace higherlower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1,101);
+            
+
+            while (true)
+            {
+                Console.Write("input a number: ");
+                int user_input = Convert.ToInt16(Console.ReadLine());
+                if (user_input == num)
+                {
+                    Console.WriteLine("you win");
+                    break;
+                }
+                else if (user_input > num)
+                {
+                    Console.WriteLine("lower");
+                }
+                else if (user_input < num)
+                {
+                    Console.WriteLine("higher");
+                }
+
+            }
+        }
+    }
+}
